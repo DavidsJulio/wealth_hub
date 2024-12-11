4eca711d2fb1b95c5a8e425c483b20e3fe63bc05
DavidJulio97 <david.julio@mobilu.eu>
2024-12-11 21:29:02 +0000

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const path = require("path");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias["web_kit_ui"] = path.resolve(
+      __dirname,
+      "../web_kit_ui/src"
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
